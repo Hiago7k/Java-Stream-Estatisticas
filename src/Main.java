58d9c7ae4a3f22c0292e0d9cfeb7cc196909e4ab
Hiago 7k <hiago7k@gmail.com>
2026-08-26 18:16:42 -0300

    filtrando primeira lista

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Main{
     public static void main(String[] args){
-        System.out.println("Hello");
+        /*
+         1 - Dada a lista de números inteiros a seguir,
+          encontre o maior número dela.
+           */
+        List<Integer> numeros = Arrays.asList(10, 20, 30, 40, 50);
+
+        IntSummaryStatistics est = numeros.stream()
+          .collect(Collectors.summarizingInt(Integer::intValue));
+        System.out.println("O maior número da lista é: " + est.getMax());
+
     }
 }
