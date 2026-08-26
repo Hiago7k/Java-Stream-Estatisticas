7461af403d5729e4a793b0a4ce14498197e8a88e
Hiago 7k <hiago7k@gmail.com>
2026-08-26 18:37:07 -0300

    map

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 class Main{
@@ -13,7 +14,23 @@ class Main{
 
         IntSummaryStatistics est = numeros.stream()
           .collect(Collectors.summarizingInt(Integer::intValue));
-        System.out.println("O maior número da lista é: " + est.getMax());
+        //System.out.println("O maior número da lista é: " + est.getMax());
+
+        /*
+            2 -Dada a lista de palavras (strings) abaixo, agrupe-as
+            pelo seu tamanho. No código a seguir, há um exemplo prático
+            do resultado esperado.
+            Resultado Esperado: {4=[java, code], 6=[stream, lambda]}
+       */
+        List<String> palavras = Arrays.asList("java", "stream", "lambda", "code");
+
+        Map<Integer, String> stringsAgrupadas = palavras.stream()
+                .collect(Collectors.toCollection(palavras::getClass,
+                        Collectors.averagingDouble(palavras::sort)));
+
+
+        System.out.println(stringsAgrupadas);
+
 
     }
 }
