832f2702ed4e737e0b70af59ca0f664b4440182c
Hiago 7k <hiago7k@gmail.com>
2026-08-27 17:28:07 -0300

    corringo construtor

--- src/Main.java
+++ src/Main.java
@@ -61,5 +61,18 @@ class Main{
        // System.out.println("Pares " + separando.get(true));
        // System.out.println("Imapres " + separando.get(false));
 
+
+
+        // Lista de produtos
+
+
+        List<Produto> produtos = Arrays.asList(
+                new Produto("Smartphone", 800.0, "Eletrônicos"),
+                new Produto("Notebook", 1500.0, "Eletrônicos"),
+                new Produto("Teclado", 200.0, "Eletrônicos"),
+                new Produto("Cadeira", 300.0, "Móveis"),
+                new Produto("Monitor", 900.0, "Eletrônicos"),
+                new Produto("Mesa", 700.0, "Móveis")
+        );
     }
 }
